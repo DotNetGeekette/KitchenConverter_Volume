028c5de4e4483e608447e9e2f88cb8b00c700950
Jamie Colley <jamie.colley.88@gmail.com>
2013-05-17 17:34:25 -0400

    Initial Commit plus number buttons

---
+++ KitchenConverter_Volume/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using KitchenConverter_Volume.Resources;
+
+namespace KitchenConverter_Volume
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
