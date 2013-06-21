955f93f703b5dc964d8b629249910318454e8e8c
Jamie Colley <jamie.colley.88@gmail.com>
2013-06-21 08:41:26 -0400

    The bulk of the app
    Adding an about page, actually doing the calculations

---
+++ KitchenConverter_Volume/About.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Microsoft.Phone.Tasks;
+
+namespace KitchenConverter_Volume
+{
+    public partial class About : PhoneApplicationPage
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        public void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        public void ReviewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var review = new MarketplaceReviewTask();
+            review.Show();
+        }
+
+        public void OtherAppsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var search = new MarketplaceSearchTask();
+            search.SearchTerms = "DotNetGeekette";
+            search.Show();
+        }
+
+        public void EmailBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var email = new EmailComposeTask();
+            email.Subject = "Kitchen Converter - Volume Edition (v1.0)";
+            email.Body = String.Empty;
+            email.To = "jamie@dotnetgeekette.com";
+            email.Show();
+        }
+
+        public void BlogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("http://www.dotnetgeekette.com", UriKind.Absolute);
+            webBrowserTask.Show();
+        }
+
+        public void TwitterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri("http://www.twitter.com/dotnetgeekette", UriKind.Absolute);
+            webBrowserTask.Show();
+        }
+
+       
+    }
+}
--- KitchenConverter_Volume/MainPage.xaml.cs
+++ KitchenConverter_Volume/MainPage.xaml.cs
@@ -13,19 +13,98 @@ namespace KitchenConverter_Volume
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
         public MainPage()
         {
-            
             InitializeComponent();
-            //this.UnitNames.ItemsSource = units;
-            // Sample code to localize the ApplicationBar
-            //BuildLocalizedApplicationBar();
+
         }
 
         public void Recalculate(object sender, RoutedEventArgs e)
         {
-            var x = 1;
+            double number;
+            if (Number != null)
+            {
+                if (String.IsNullOrEmpty(Number.Text))
+                {
+                    equalsTsp.Text = String.Empty;
+                    equalsTbsp.Text = String.Empty;
+                    equalsCup.Text = String.Empty;
+                }
+                else if (!Double.TryParse(Number.Text, out number))
+                {
+                    equalsTsp.Text = "Error";
+                    equalsTbsp.Text = "Error";
+                    equalsCup.Text = "Error";
+                }
+                else if (number > 100000)
+                {
+                    equalsTsp.Text = "Too many";
+                    equalsTbsp.Text = "Too many";
+                    equalsCup.Text = "Too many";
+                }
+                else
+                {
+                    switch (((ListPickerItem)Units.SelectedItem).Name)
+                    {
+                        case "tsp":
+                            {
+                                ConvertFromTeaspoons(number);
+                                break;
+                            }
+                        case "tbsp":
+                            {
+                                ConvertFromTablespoons(number);
+                                break;
+                            }
+                        case "cup":
+                            {
+                                ConvertFromCups(number);
+                                break;
+                            }
+
+                    }
+                }
+            }
+        }
+
+        public void AboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/About.xaml", UriKind.Relative));
+        }
+
+        private void ConvertFromTeaspoons(double quantity)
+        {
+            equalsTsp.Text = FormatNumber(quantity);
+            equalsTbsp.Text = FormatNumber(quantity / 3);
+            equalsCup.Text = FormatNumber(quantity / 48);
+        }
+
+        private void ConvertFromTablespoons(double quantity)
+        {
+            equalsTsp.Text = FormatNumber(quantity * 3);
+            equalsTbsp.Text = FormatNumber(quantity);
+            equalsCup.Text = FormatNumber(quantity * 16);
+        }
+
+        private void ConvertFromCups(double quantity)
+        {
+            equalsTsp.Text = FormatNumber(quantity * 48);
+            equalsTbsp.Text = FormatNumber(quantity * 16);
+            equalsCup.Text = FormatNumber(quantity);
+        }
+
+        public string FormatNumber(double myNumber)
+        {
+            var s = string.Format("{0:0.00}", myNumber);
+
+            if (s.EndsWith("00"))
+            {
+                return ((int)myNumber).ToString();
+            }
+            else
+            {
+                return s;
+            }
         }
     }
 }
